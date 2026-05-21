2ce4c61f032d4de0769ce4021460012d91a7b09c
Marcelo Lopes Da Silva <marcelol527319@gmail.com>
2026-05-21 02:51:59 -0300

    Atualizando imagens e estrutura dos produtos

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'monte-sinai-20260521-15';
+const CACHE_VERSION = 'monte-sinai-20260521-17';
 const STATIC_CACHE = `${CACHE_VERSION}-static`;
 const PAGE_CACHE = `${CACHE_VERSION}-pages`;
 
@@ -9,8 +9,8 @@ const STATIC_ASSETS = [
   '/promocoes',
   '/sobre',
   '/contato',
-  '/css/style.css?v=20260521-15',
-  '/js/script.js?v=20260521-15',
+  '/css/style.css?v=20260521-17',
+  '/js/script.js?v=20260521-17',
   '/js/supabase.js?v=20260520-4',
   '/site.webmanifest',
   '/assets/brand/monte-sinai-logo-3d.png',
